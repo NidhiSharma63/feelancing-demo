8bb58e1aabdf0173d113e318349eba8fc4cd83af
NidhiSharma02 <nidhi.sharma@zeitview.com>
2025-08-20 12:00:36 +0530

    WIP

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import gsap from "gsap";
+import React,{ useRef } from "react";
 const Home = () => {
+  const solutionRef = useRef(null);
+  const productRef = useRef(null);
+
+  const buttonRefFirst = useRef(null);
+  const buttonRefSecond = useRef(null);
+
+  const whiteScreenRef = useRef(null);
+  const handleExpand = (ref) => {
+    gsap.to(ref.current, {
+      width: "100vw",
+      height: "100vh",
+      top: 0,
+      left: 0,
+      borderRadius: 0,
+      duration: 1, // 1 second animation
+      ease: "power3.inOut",
+      zIndex: 1,
+      onComplete:()=>{
+        buttonRefFirst.current.style.display = "none";
+        buttonRefSecond.current.style.display = "none";
+        gsap.to(whiteScreenRef.current, {
+          width: "100vw",
+          height: "100vh",
+          top: 0,
+          left: 0,
+          borderRadius: 0,
+          duration: 1, // 1 second animation
+          ease: "power3.inOut",
+          zIndex:2
+        })
+      }
+    });
+    gsap.to(buttonRefFirst.current, {
+      duration: 1, // 1 second animation
+      ease: "power3.inOut",
+      right: "-1000%",
+    });
+    gsap.to(buttonRefSecond.current, {
+       right: "-1000%",
+      duration: 1, // 1 second animation
+      ease: "power3.inOut",
+    });
+  };
   return (
-    <div className="flex h-screen">
-      {/*  */}
+    <div className="flex h-screen overflow-hidden">
+      {/* white screen */}
+      <div ref={whiteScreenRef} className="absolute w-[0px] h-[0px] bg-white bottom-0 left-0"/>
       <div className="flex flex-col gap-3 w-1/2  mt-40 ml-20 pb-10 justify-around border-2 border-amber-400">
         <h1 className="text-4xl font-bold">GENTHERM</h1>
         <div className="text-8xl ">
@@ -10,18 +55,33 @@ const Home = () => {
           <p>To Next Degree</p>
         </div>
         <div className="flex gap-6 ">
-          <button className="cursor-pointer w-[200px] py-[10px] text-white text-sm">
+          <button
+            onClick={() => handleExpand(solutionRef)}
+            className="cursor-pointer w-[200px] py-[10px] text-black text-sm"
+          >
             Explore by
-            <div className="absolute  bg-green-700  z-[-1]  w-[200px] h-[68px] top-[80.3%]"></div>
+            <div
+              ref={solutionRef}
+              className="absolute  bg-green-700  z-[-1]  w-[200px] h-[68px] top-[80.3%]"
+            />
             <hr className="text-green-700" />
-            <p className="text-xl">Solution</p>
+            <p className="text-xl relative" ref={buttonRefFirst}>
+              Solution
+            </p>
           </button>
-          <button className=" cursor-pointer text-white w-[200px] text-sm">
+          <button
+            onClick={() => handleExpand(productRef)}
+            className="cursor-pointer text-black w-[200px] text-sm"
+          >
             Explore by
-            <div className="absolute  bg-green-700  z-[-1]  w-[200px] h-[68px] top-[80.3%]"></div>
-
+            <div
+              ref={productRef}
+              className="absolute  bg-green-700  z-[-1]  w-[200px] h-[68px] top-[80.3%]"
+            />
             <hr className="text-green-700" />
-            <p className="text-xl">Products</p>
+            <p className="text-xl relative" ref={buttonRefSecond}>
+              Products
+            </p>
           </button>
         </div>
       </div>
